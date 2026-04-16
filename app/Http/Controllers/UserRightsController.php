0c22300dac3e1afac85724b8c6907199872620f8
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-04-17 00:40:30 +0300

    Оптимизация sql при работе с правами

--- app/Http/Controllers/UserRightsController.php
+++ app/Http/Controllers/UserRightsController.php
@@ -14,7 +14,9 @@ class UserRightsController extends Controller
     public function manage_global_user_rights(int $userId) {
         $managed_user = User::findOrFail($userId);
         $user_groups = UserGroup::where('is_global', 1)->get();
-        $user_user_group_wiki = UserUserGroupWiki::where('wiki_id', 0)->get();
+        $user_user_group_wiki = UserUserGroupWiki::where('wiki_id', 0)
+            ->where('user_id', $userId)
+            ->get();
 
         return view('manage_user_groups', compact('managed_user', 'user_groups', 'user_user_group_wiki'));
     }
@@ -25,47 +27,33 @@ class UserRightsController extends Controller
             'user_group_ids' => 'array',
         ]);
 
-        $managed_user = User::findOrFail($userId);//ищем управляемого участника
-        $user_user_group_wiki = UserUserGroupWiki::where('user_id', $userId)
+        User::findOrFail($userId);//ищем управляемого участника
+        $currentUserGroupIds = UserUserGroupWiki::where('user_id', $userId)
             ->where('wiki_id', 0)
-            ->get();
-        //получаем текущие группы
-        $user_user_group_wiki_ids = [];
+            ->pluck('user_group_id')
+            ->all();
 
-        for ($i = 0; $i < count($user_user_group_wiki); $i++) {
-            array_push($user_user_group_wiki_ids, $user_user_group_wiki[$i]->user_group_id);
-        }
+        $user_group_ids = array_map('intval', $data['user_group_ids'] ?? []);
+        $user_group_ids_to_remove = array_values(array_diff($currentUserGroupIds, $user_group_ids));
+        $user_group_ids_to_add = array_values(array_diff($user_group_ids, $currentUserGroupIds));
 
-        if(isset($data['user_group_ids'])) {
-            $user_group_ids = $data['user_group_ids'];
-        }
-        else {
-            $user_group_ids = [];
-        }
-
-        for ($i = 0; $i < count($user_group_ids); $i++) {
-            $user_group_ids[$i] = (int)$user_group_ids[$i];
-        }
-
-        $user_group_ids_to_remove = array_diff($user_user_group_wiki_ids, $user_group_ids);
-        $user_group_ids_to_add = array_diff($user_group_ids, $user_user_group_wiki_ids);
-
-        $user_group_ids_to_remove = array_values($user_group_ids_to_remove);
-        $user_group_ids_to_add = array_values($user_group_ids_to_add);
-
-        for ($i = 0; $i < count($user_group_ids_to_remove); $i++) {
+        if (count($user_group_ids_to_remove) > 0) {
             UserUserGroupWiki::where('user_id', $userId)
                 ->where('wiki_id', 0)
-                ->where('user_group_id', $user_group_ids_to_remove[$i])
+                ->whereIn('user_group_id', $user_group_ids_to_remove)
                 ->delete();
         }
-        for ($i = 0; $i < count($user_group_ids_to_add); $i++) {
-            $one_user_group_to_add = [
-                'user_id' => $userId,
-                'user_group_id' => $user_group_ids_to_add[$i],
-                'wiki_id' => 0,
-            ];
-            UserUserGroupWiki::create($one_user_group_to_add);
+
+        if (count($user_group_ids_to_add) > 0) {
+            $rowsToInsert = array_map(static function ($groupId) use ($userId) {
+                return [
+                    'user_id' => $userId,
+                    'user_group_id' => $groupId,
+                    'wiki_id' => 0,
+                ];
+            }, $user_group_ids_to_add);
+
+            UserUserGroupWiki::insert($rowsToInsert);
         }
 
         return response(__('The user\'s usergroups was changed'), 200)
@@ -81,7 +69,9 @@ class UserRightsController extends Controller
         if ($wiki) {
             $managed_user = User::findOrFail($userId);
             $user_groups = UserGroup::where('is_global', 0)->get();
-            $user_user_group_wiki = UserUserGroupWiki::where('wiki_id', $wiki->id)->get();
+            $user_user_group_wiki = UserUserGroupWiki::where('wiki_id', $wiki->id)
+                ->where('user_id', $userId)
+                ->get();
 
             return view('manage_local_user_groups', compact('wiki', 'managed_user', 'user_groups', 'user_user_group_wiki'));
         }
@@ -95,46 +85,34 @@ class UserRightsController extends Controller
             $data = request()->validate([
                 'user_group_ids' => 'array',
             ]);
-            $managed_user = User::findOrFail($userId);
-            $user_user_group_wiki = UserUserGroupWiki::where('user_id', $userId)
+            User::findOrFail($userId);
+
+            $currentUserGroupIds = UserUserGroupWiki::where('user_id', $userId)
                 ->where('wiki_id', $wiki->id)
-                ->get();
-            $user_user_group_wiki_ids = [];
+                ->pluck('user_group_id')
+                ->all();
 
-            for ($i = 0; $i < count($user_user_group_wiki); $i++) {
-                array_push($user_user_group_wiki_ids, $user_user_group_wiki[$i]->user_group_id);
-            }
+            $user_group_ids = array_map('intval', $data['user_group_ids'] ?? []);
+            $user_group_ids_to_remove = array_values(array_diff($currentUserGroupIds, $user_group_ids));
+            $user_group_ids_to_add = array_values(array_diff($user_group_ids, $currentUserGroupIds));
 
-            if(isset($data['user_group_ids'])) {
-                $user_group_ids = $data['user_group_ids'];
-            }
-            else {
-                $user_group_ids = [];
-            }
-
-            for ($i = 0; $i < count($user_group_ids); $i++) {
-                $user_group_ids[$i] = (int)$user_group_ids[$i];
-            }
-
-            $user_group_ids_to_remove = array_diff($user_user_group_wiki_ids, $user_group_ids);
-            $user_group_ids_to_add = array_diff($user_group_ids, $user_user_group_wiki_ids);
-
-            $user_group_ids_to_remove = array_values($user_group_ids_to_remove);
-            $user_group_ids_to_add = array_values($user_group_ids_to_add);
-
-            for ($i = 0; $i < count($user_group_ids_to_remove); $i++) {
+            if (count($user_group_ids_to_remove) > 0) {
                 UserUserGroupWiki::where('user_id', $userId)
                     ->where('wiki_id', $wiki->id)
-                    ->where('user_group_id', $user_group_ids_to_remove[$i])
+                    ->whereIn('user_group_id', $user_group_ids_to_remove)
                     ->delete();
             }
-            for ($i = 0; $i < count($user_group_ids_to_add); $i++) {
-                $one_user_group_to_add = [
-                    'user_id' => $userId,
-                    'user_group_id' => $user_group_ids_to_add[$i],
-                    'wiki_id' => $wiki->id,
-                ];
-                UserUserGroupWiki::create($one_user_group_to_add);
+
+            if (count($user_group_ids_to_add) > 0) {
+                $rowsToInsert = array_map(static function ($groupId) use ($userId, $wiki) {
+                    return [
+                        'user_id' => $userId,
+                        'user_group_id' => $groupId,
+                        'wiki_id' => $wiki->id,
+                    ];
+                }, $user_group_ids_to_add);
+
+                UserUserGroupWiki::insert($rowsToInsert);
             }
 
             return response(__('The user\'s usergroups was changed'), 200)
