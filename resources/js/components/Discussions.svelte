8cd3964bba4bd9cafc74b9503f9d1f373c40af7b
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-04-30 04:45:03 +0300

    Строим вершнюю часть страницы с обсуждениями

--- resources/js/components/Discussions.svelte
+++ resources/js/components/Discussions.svelte
@@ -1,6 +1,22 @@
 <script>
-    console.log('Обсуждения загружены');
+    let { wikiName, userId, userName, userModerateDiscussions } = $props();
 </script>
 <div>
-    Привет, обсуждения
+    <div class="editor-placeholder">
+        <div class="avatar-wrapper">
+            {#if userId !== 0}
+                <div></div>
+            {:else}
+                <div></div>
+            {/if}
+        </div>
+    </div>
 </div>
+<style>
+.editor-placeholder {
+    border: 1px solid;
+    border-radius: 10px;
+    height: 100px;
+    background-color: var(--bs-tertiary-bg);
+}
+</style>
