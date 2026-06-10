28ac40313a88aa84427e41d317d681ed57bdbda0
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-06-10 07:05:35 +0300

    Transfer articles feature completed

--- routes/web.php
+++ routes/web.php
@@ -125,7 +125,7 @@ Route::middleware([ProtectionLevel3Middleware::class])->group(function () {
     Route::post('/transfer-articles/article_id/{article}', [ArticleController::class,'transfer_articles'])
     ->name('articles.transfer.form')
     ->middleware(TransferArticlesMiddleware::class);
-    Route::post('/transfer-articles-post', [ArticleController::class,'transfer_articles_post'])
+    Route::post('/transfer-articles-post/article_id/{article}', [ArticleController::class,'transfer_articles_post'])
     ->name('articles.transfer.post')
     ->middleware(TransferArticlesMiddleware::class);
 
