ebdb93e11373fe6f878e06756b61978fc8d389d5
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-06-15 15:39:50 +0300

    Тесты для профиля участника

---
+++ tests/Feature/UserProfileTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Wiki;
+
+class UserProfileTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_that_global_profile_is_assessable(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->get("userprofile-global/{$user->id}");
+
+        $response->assertStatus(200);
+    }
+
+    public function test_that_local_is_assessable(): void
+    {
+        $user = User::factory()->create();
+
+        $wiki = Wiki::factory()->create();
+
+        $response = $this->get("/wiki/{$wiki->url}/userprofile/{$user->id}");
+
+        $response->assertStatus(200);
+    }
+}
