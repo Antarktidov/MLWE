1926e4f22c5b108ffa81522950a2696a2be29a3f
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-06-15 14:25:08 +0300

    Тесты для друзей

--- tests/Feature/CategoriesPageTest.php
+++ tests/Feature/CategoriesPageTest.php
@@ -16,20 +16,6 @@ class CategoriesPageTest extends TestCase
     {
         $wiki = Wiki::factory()->create();
 
-        /*$article = Article::factory()->create([
-            'wiki_id' => $wiki->id,
-        ]);*/
-
-        /*$revision = Revision::factory()->create([
-            'article_id' => $article->id,
-            'url_title' => $article->url_title,
-            'title' => $article->title,
-            'user_id' => 0,
-            'user_ip' => '127.0.0.1',
-            'is_approved' => true,
-            'is_patrolled' => true,
-        ]);*/
-
         $category = Category::factory()->create();
 
         $response = $this->get("/wiki/{$wiki->url}/category/{$category->name}");
