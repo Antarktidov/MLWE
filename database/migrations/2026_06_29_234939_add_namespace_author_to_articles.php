5664194db0a7e8c991fda3cafc544e146b17f42d
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-06-30 02:54:12 +0300

    Новый тип статьи (блог)

---
+++ database/migrations/2026_06_29_234939_add_namespace_author_to_articles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('namespace')->default('article');
+            $table->unsignedBigInteger('author_id')->nullable();//if it's blog (not regular article)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('namespace');
+            $table->dropColumn('author_id');
+        });
+    }
+};
