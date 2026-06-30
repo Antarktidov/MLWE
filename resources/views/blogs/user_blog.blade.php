5d9c25a0e4168e56ba19a2e14d2c319f11223a41
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-06-30 15:27:18 +0300

    Страница с блогом учасмтника готова

--- resources/views/blogs/user_blog.blade.php
+++ resources/views/blogs/user_blog.blade.php
@@ -3,14 +3,27 @@
 <style>
 .blog {
     border: 1px solid;
-    padding: 10px;
+    max-height: 500px;
+    overflow: clip;
+    position: relative;
     margin-bottom: 5px;
 }
+
+.blog__inner {
+    height: 100%;
+    overflow: clip;
+    padding: 10px;
+
+    mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+    -webkit-mask-image: linear-gradient(to bottom, black 80%, transparent 100%);
+}
+
 </style>
 <h1>Блог учатника: {{$author->name}}</h1>
 @foreach ($articles as $blog)
 <section>
     <div class="blog">
+        <div class="blog__inner">
         <a href="{{ route('blogs.show', [$wiki->url, $blog->author_id, $blog->url_title]) }}">
             <h2>{{$blog->title}}</h2>
             <div class="content">@php
@@ -22,6 +35,7 @@
         @endphp
         {!! $content !!}</div>
         </a>
+        </div>
     </div>
 </section>
 @endforeach
