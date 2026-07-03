4721bc595f9ea3e68ada9821b7e0ab8f5bff5d9e
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-07-03 23:03:02 +0300

    12

--- resources/views/html/show.blade.php
+++ resources/views/html/show.blade.php
@@ -21,9 +21,8 @@
     <h1>HTML:{{ $revision->title }}</h1>
     <div class="links">
         @if ($canEditHtmlPages)
-            <a href="{{ route('html.edit', [$wiki->url, $author->id, $article->url_title]) }}" class="btn btn-primary">{{ __('Edit') }}</a>
-            <a href="{{ route('html.history', [$wiki->url, $author->id, $article->url_title]) }}" class="btn btn-secondary">{{ __('History') }}</a>
-            <form action="{{ route('html.destroy', [$wiki->url, $author->id, $article->url_title]) }}" method="post">
+            <a href="{{ route('html.edit', [$wiki->url, $article->url_title]) }}" class="btn btn-primary">{{ __('Edit') }}</a>
+            <form action="{{ route('html.destroy', [$wiki->url, $article->url_title]) }}" method="post">
                 @csrf
                 @method('delete')
                 <button class="btn btn-danger" type="submit">{{ __('Delete') }}</button>
