4f44fdbdb4c37fd2fc2d81c9bb151e8d295feee6
Antarktidov <96629481+Antarktidov@users.noreply.github.com>
2026-09-12 17:42:16 +0300

    Новая миграция + форма

--- resources/views/create-thread.blade.php
+++ resources/views/create-thread.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 @section('content')
 <h1>Новое сообщение в категории форума: {{$article->title}}</h1>
+<form action="#">
+    @csrf
+    <div class="mb-3">
+      <label for="title" class="form-label">{{__('Title')}}</label>
+      <input type="text" name="title" class="form-control" id="title">
+    </div>
+    <div class="mb-3">
+        <label  class="form-label" for="content">{{__('Message text')}}</label>
+        <textarea name="content" class="form-control" id="content"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">{{__('Save')}}</button>
+  </form>
 @endsection
